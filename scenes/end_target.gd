c91b328ce656b3c8b9316370dfadf19bd7f86378
flapp <gaetanandrekeller@gmail.com>
2026-05-02 18:19:38 +0200

    End level triggerr

---
+++ scenes/end_target.gd
@@ -0,0 +1,7 @@
+extends "res://scripts/end_target.gd"
+
+@export var next_scene: String = "res://scenes/level_test.tscn"
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file(next_scene)
